f57e8fa7f1cbcafeffaf1cb78704bf93b14a1424
patient-fyd <patient.fyd@gmail.com>
2023-04-10 09:43:26 +0800

    ·

--- bitjavase/test/src/HasStatic.java
+++ bitjavase/test/src/HasStatic.java
@@ -7,11 +7,11 @@ public class HasStatic {
     private static int x = 100;
     public static void main(String args[]) {
         HasStatic hs1 = new HasStatic();
-        hs1.x++;
+        x++;
         HasStatic hs2 = new HasStatic();
-        hs2.x++;
+        x++;
         hs1 = new HasStatic();
-        hs1.x++;
+        x++;
         HasStatic.x--;
         System.out.println(" x=" + x);
     }
---
+++ bitjavase/test/src/test/Computer.java
@@ -0,0 +1,42 @@
+package test;
+
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description:
+ * @DateTime: 2023/4/10 9:36
+ */
+public class Computer {
+    private String cpu;
+    private int memory;
+    private int disk;
+
+    public Computer(String cpu, int memory, int disk) {
+        this.cpu = cpu;
+        this.memory = memory;
+        this.disk = disk;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public int getDisk() {
+        return disk;
+    }
+
+    public void setDisk(int disk) {
+        this.disk = disk;
+    }
+}
