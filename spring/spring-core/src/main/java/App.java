841ec46223d4df56ae781937d44c1d64bcacd4af
patient-fyd <patient.fyd@gmail.com>
2023-04-13 13:45:19 +0800

    `

--- spring/spring-core/src/main/java/App.java
+++ spring/spring-core/src/main/java/App.java
@@ -1,3 +1,6 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @Author: Fourteen-Y
  * @Description:
@@ -5,6 +8,14 @@
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // 1.得到Spring的上下文对象，创建的时候需要配置Spring配置信息
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+
+        // 2.加载某个bean
+        User user = (User) context.getBean("user");
+
+        // 3.调用方法
+        System.out.println(user.sayHi("Fourteen-Y"));
     }
 }
