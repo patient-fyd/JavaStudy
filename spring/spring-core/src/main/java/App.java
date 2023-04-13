d51bb98af58d8f866affd8f559091ef83d2984d1
patient-fyd <patient.fyd@gmail.com>
2023-04-13 09:56:50 +0800

    spring-core

---
+++ spring/spring-core/src/main/java/App.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Fourteen-Y
+ * @Description:
+ * @Date: 2023/4/13 9:17
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
