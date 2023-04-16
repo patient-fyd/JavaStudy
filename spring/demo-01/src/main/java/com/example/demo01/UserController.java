5418998485dc3dcaf30648b877d5a62d3882f5ab
patient-fyd <patient.fyd@gmail.com>
2023-04-16 19:50:51 +0800

    demo01
    spring2.7

---
+++ spring/demo-01/src/main/java/com/example/demo01/UserController.java
@@ -0,0 +1,18 @@
+package com.example.demo01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Fourteen-Y
+ * @Description:
+ * @Date: 2023/4/16 19:31
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/sayhi")
+    public String sayHi(){
+        return "Hi,Spring Boot.";
+    }
+}
