28601d6a7a48b115f8b321ec36aaa4581f51cca0
patient-fyd <patient.fyd@gmail.com>
2023-04-18 00:15:09 +0800

    spring-config

---
+++ spring/spring-demo1/src/main/java/App.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description:
+ * @DateTime: 2023/4/17 23:58
+ */
+public class App {
+    public static void main(String[] args) {
+        // 后续实现
+    }
+}
---
+++ spring/spring-demo1/src/main/java/User.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description:
+ * @DateTime: 2023/4/18 0:06
+ */
+public class User {
+    public  void sayHi() {
+        System.out.println("Hi");
+    }
+}
