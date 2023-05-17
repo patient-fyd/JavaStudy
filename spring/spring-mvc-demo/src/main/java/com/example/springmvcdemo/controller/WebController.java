068bc8bf3281c2d471e45145e4582638b8fc2244
patient-fyd <patient.fyd@gmail.com>
2023-05-18 01:06:51 +0800

    获取文件

--- spring/spring-mvc-demo/src/main/java/com/example/springmvcdemo/controller/WebController.java
+++ spring/spring-mvc-demo/src/main/java/com/example/springmvcdemo/controller/WebController.java
@@ -4,9 +4,12 @@ import com.example.springmvcdemo.model.Student;
 import com.example.springmvcdemo.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -22,86 +25,105 @@ public class WebController {
     //@RequestMapping(value = "/hi",method = RequestMethod.GET)
     @GetMapping("/hi")
     //@PostMapping("/hi")
-    public Object sayHi(HttpServletRequest request, HttpServletResponse response){
+    public Object sayHi(HttpServletRequest request, HttpServletResponse response) {
         return "hi," + request.getParameter("name");
     }
 
     /**
      * 获取单个参数
+     *
      * @return
      */
     @GetMapping("/get1")
-    public String getParam1(String name){
+    public String getParam1(String name) {
         return "value:  " + name;
     }
 
     /**
      * 获取多个参数
+     *
      * @param name
      * @param age
      * @return
      */
     @GetMapping("/get2")
-    public String getParam2(String name,Integer age){
+    public String getParam2(String name, Integer age) {
         return "name:" + name + " age: " + age;
     }
 
     /**
      * 获取对象参数
+     *
      * @param student
      * @return
      */
     @GetMapping("/get3")
-    public String getParam3(Student student){
+    public String getParam3(Student student) {
         return student.toString();
     }
 
     /**
      * 获取form表单中的参数
+     *
      * @param username
      * @param password
      * @return
      */
     @PostMapping("/login")
-    public String login(String username,String password){
+    public String login(String username, String password) {
         return "<h1>用户名" + username + "密码" + password + "</h1>";
     }
 
     /**
      * 获取form表单中的参数
+     *
      * @param username
      * @param password
      * @return
      */
     @RequestMapping("/login2")
-    public HashMap<String, Object> login2(String username, String password){
+    public HashMap<String, Object> login2(String username, String password) {
         HashMap<String, Object> map = new HashMap<>();
-        map.put("username",username);
-        map.put("password",password);
+        map.put("username", username);
+        map.put("password", password);
         return map;
     }
 
     /**
      * 得到用户传递的json数据
+     *
      * @param user
      * @return
      */
     @RequestMapping("/login3")
-    public HashMap<String, Object> login3(@RequestBody User user){
+    public HashMap<String, Object> login3(@RequestBody User user) {
         HashMap<String, Object> map = new HashMap<>();
-        map.put("username",user.getUsername());
-        map.put("password",user.getPassword());
+        map.put("username", user.getUsername());
+        map.put("password", user.getPassword());
         return map;
     }
 
     /**
      * 获取form表单（多个参数）
+     *
      * @param user
      * @return
      */
     @RequestMapping("/reg")
-    public String reg(User user){
+    public String reg(User user) {
         return user.toString();
     }
 
+    /**
+     * 上传文件
+     */
+    @RequestMapping("/reg2")
+    public String reg2(String username, @RequestPart("myfile") MultipartFile file) throws IOException {
+        // 保存文件
+        file.transferTo(new File("C:\\Users\\33275\\Documents\\WK\\" + file.getOriginalFilename()));
+        return username + "上传了" + file.getOriginalFilename();
+    }
+
 }
+
+
