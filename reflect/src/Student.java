6c1f0c5c47899bef2250d7e9f37c3c7c2e78c8fb
patient-fyd <patient.fyd@gmail.com>
2023-12-11 11:30:53 +0800

    反射

---
+++ reflect/src/Student.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description:
+ * @DateTime: 2023/12/11 11:28
+ */
+public class Student {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ reflect/src/Test1Class.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: patient.fyd@gmail.com
+ * @Description:
+ * @DateTime: 2023/12/11 11:29
+ */
+public class Test1Class {
+    public static void main(String[] args) {
+        Class c1 = Student.class;
+        System.out.println(c1.getName());
+    }
+}
