7fe59e1da12773dccacf5f0231f05306a9c6b701
GanFengYu <patient.fyd@gmail.com>
2023-12-11 14:07:21 +0800

    获取类中的构造器

---
+++ reflect/src/Cat.java
@@ -0,0 +1,17 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Author: patient.fyd@gmail.com
+ * Date: 2023/12/11  13:53
+ * Description:
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cat {
+    private String name;
+    private int age;
+
+}
--- reflect/src/Test2Constructor.java
+++ reflect/src/Test2Constructor.java
@@ -1,7 +1,37 @@
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
 /**
  * Author: patient.fyd@gmail.com
  * Date: 2023/12/11  13:47
  * Description:
  */
 public class Test2Constructor {
+    @Test
+    public void testGetConstructors() {
+        // 1、反射第一步：获取Class对象
+        Class c = Cat.class;
+        // 2、获取所有构造方法  getConstructors()只能拿public修饰的
+        //Constructor[] constructors = c.getConstructors();
+        Constructor[] constructors = c.getDeclaredConstructors();
+        // 3、遍历数组中的每个构造器对象
+        for (Constructor constructor : constructors) {
+            System.out.println(constructor.getName() + "----->" + constructor.getParameterCount());
+        }
+    }
+
+    @Test
+    public void testGetConstructor() throws NoSuchMethodException {
+        // 1、反射第一步：获取Class对象
+        Class c = Cat.class;
+        // 2、获取某个构造方法 getConstructor()只能拿public修饰的
+        //Constructor constructor = c.getConstructor();
+        Constructor constructor = c.getDeclaredConstructor(String.class, int.class);
+        // 3、遍历数组中的每个构造器对象
+        System.out.println(constructor.getName() + "----->" +
+                constructor.getParameterCount());
+
+    }
+
 }
